4c7a55dfd67dd181cce37373bd7a04023f014c1b
Pálfy Dávid <palfydavid9601@gmail.com>
2018-12-12 13:47:21 +0100

    Added colors in order to have pretty colorful balls

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@ const context = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+let loop;
 let balls = [];
+let colors = ["#00a8ff", "#9c88ff", "#fbc531", "#4cd137", "#e84118"];
 
 class Ball {
    constructor() {
       this.radius = randomGenerator(20, 50);
+      this.color = colors[randomGenerator(0, (colors.length - 1))];
       this.x = randomGenerator(this.radius, canvas.width - this.radius);
       this.y = randomGenerator(this.radius, canvas.height - this.radius);
       this.dx = randomGenerator(-20, 20);
@@ -17,7 +20,7 @@ class Ball {
    Draw() {
       context.beginPath();
       context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-      context.fillStyle = "#000000";
+      context.fillStyle = this.color;
       context.fill();
       context.closePath();
    }
@@ -48,9 +51,8 @@ function Init() {
    }
 }
 
-let gameloop;
 function Gameloop() {
-   gameloop = requestAnimationFrame(Gameloop);
+   loop = requestAnimationFrame(Gameloop);
    context.clearRect(0, 0, canvas.width, canvas.height);
    for(let i = 0; i < balls.length; i++) {
       balls[i].Move();
